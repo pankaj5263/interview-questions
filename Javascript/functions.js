afc7c739445656c76adf92cbf5abc953503711a2
shashank-nx <shashank.dixit@successive.tech>
2024-08-27 08:16:40 +0530

    functions arrow

---
+++ Javascript/functions.js
@@ -0,0 +1,14 @@
+function sqr(num) {// this is params
+    return num * num;
+}
+
+
+sqr(5) // this is argumentz
+
+
+// arrow functions
+
+// 1. we can define the function with arrow, means we can define the function without function keyword.
+// 2. implicit return "return" keyword
+// 3. normal function can access arguments. but this arrow function can not access. like arguments[0]
+// 4. inside arrow function this keyword refers to the parent. but in normal function this refers to the global.
